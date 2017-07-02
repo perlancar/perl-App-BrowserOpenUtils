bd5766d95c3cc438e14aea31c866a9ad38dc061f
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-07-02 14:14:05 +0700

    Initial commit

---
+++ lib/App/BrowserOpenUtils.pm
@@ -0,0 +1,15 @@
+package App::BrowserOpenUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Utilities related to Browser::Open
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This distribution includes several utilities:
+
+#INSERT_EXECS_LIST
