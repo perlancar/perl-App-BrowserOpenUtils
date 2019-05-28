91a3f9340beca0d41ebe8e9c99731a64be1f3677
perlancar (on pc-office) <perlancar@gmail.com>
2019-05-28 13:22:21 +0700

    open-browser: Use Perinci::CmdLine, accept multiple URLs, add --dry-run (-n)

--- lib/App/BrowserOpenUtils.pm
+++ lib/App/BrowserOpenUtils.pm
@@ -3,6 +3,50 @@ package App::BrowserOpenUtils;
 # DATE
 # VERSION
 
+use 5.010001;
+use strict;
+use warnings;
+use Log::ger;
+
+our %SPEC;
+
+$SPEC{open_browser} = {
+    v => 1.1,
+    args => {
+        urls => {
+            'x.name.is_plural' => 1,
+            'x.name.singular' => 'url',
+            schema => ['array*', of=>'str*'],
+            req => 1,
+            pos => 0,
+            slurpy => 1,
+        },
+        all => {
+            schema => 'true*',
+        },
+    },
+    features => {
+        dry_run => 1,
+    },
+};
+sub open_browser {
+    require Browser::Open;
+
+    my %args = @_;
+    for my $url (@{ $args{urls} }) {
+        if ($args{-dry_run}) {
+            my $cmd = $args{all} ?
+                Browser::Open::open_browser_cmd_all() :
+                Browser::Open::open_browser_cmd();
+            log_info "[DRY_RUN] Opening %s in browser with: %s ...", $url, $cmd;
+        } else {
+            log_trace "Opening %s in browser ...", $url;
+            Browser::Open::open_browser($url, $args{all});
+        }
+    }
+    [200];
+}
+
 1;
 # ABSTRACT: Utilities related to Browser::Open
 
